d4ed2eae223f7138e3169fb2085127a41ce5d1aa
Tony Shangkuan @ Desktop <tonyshangkuan@gmail.com>
2026-07-04 00:19:09 +0800

    git init

---
+++ src/test/java/com/example/myspringaiunittest/MySpringAiUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myspringaiunittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringAiUnitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
